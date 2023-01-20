d4f0bf8c7528c24ebd5fcb08cc57ff0d4a579ffc
Goopy Flux <goopy.flux@gmail.com>
2023-01-19 21:23:47 -0600

    Convert notebooks to stand-alone scripts for memory profiling. Merge changes from remote.

--- notebooks/nondistributed_training.py
+++ notebooks/nondistributed_training.py
@@ -1,4 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Use HuggingFace Accelerate to Train Model on Temporal (monthly) Sentinel Data
+
+# ## Imports
+
+# In[1]:
+
+
 from time import time
+
+
+# In[2]:
+
 
 from accelerate import Accelerator, notebook_launcher, init_empty_weights
 from accelerate.utils import set_seed
@@ -13,10 +27,46 @@ import torch
 import torch.nn as nn
 from torch.utils.data import random_split, DataLoader
 from transformers import TrainingArguments, Trainer, logging
-from tqdm import tqdm
+from tqdm.notebook import tqdm
 
-@profile
-def get_dataloaders(chip_ids, dataset: str, batch_size: int=8, num_workers: int=4):
+
+# In[3]:
+
+
+logging.set_verbosity_error()
+
+
+# ## Utility Functions
+
+# In[4]:
+
+
+# Utility functions for printing GPU utilization
+def print_gpu_utilization():
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    info = nvmlDeviceGetMemoryInfo(handle)
+    print(f"GPU memory occupied: {info.used//1024**2} MB.")
+
+
+def print_summary(result):
+    print(f"Time: {result.metrics['train_runtime']:.2f}")
+    print(f"Samples/second: {result.metrics['train_samples_per_second']:.2f}")
+    print_gpu_utilization()
+
+
+# In[5]:
+
+
+print_gpu_utilization()
+
+
+# ## Dataset and DataLoaders
+
+# In[6]:
+
+
+def get_dataloaders(dataset: str, batch_size: int=16, num_workers: int=0):
     """Return train and eval DataLoaders with specified batch size.
     
     dataset: str
@@ -32,41 +82,41 @@ def get_dataloaders(chip_ids, dataset: str, batch_size: int=8, num_workers: int=
     else:
         data_url = ""
 
-    random_perm = np.random.permutation(len(chip_ids))
-    cut = int(0.8 * len(chip_ids))
-    train_split = random_perm[:cut]
-    eval_split = random_perm[cut:]
-
-    train_ds = eval_ds = None
     if dataset == "Sentinel-1A": # Sentinel-1 Ascending only
         ds = TemporalSentinel1Dataset(data_url=data_url, bands=["VVA", "VHA"])
     elif dataset == "Sentinel-1D": # Sentinel-1 Descending only
         ds = TemporalSentinel1Dataset(data_url=data_url, bands=["VVD", "VHD"])
     elif dataset == "Sentinel-2all":
-        train_ds = TemporalSentinel2Dataset([chip_ids[i] for i in train_split], data_url=data_url)
-        eval_ds = TemporalSentinel2Dataset([chip_ids[i] for i in eval_split], data_url=data_url)
+        ds = TemporalSentinel2Dataset(data_url=data_url)
     else:
         print("Unrecognized dataset identifier. Must be one of 'Sentinel-1A', 'Sentinel-1D' or 'Sentinel-2all'")
         return None, None
 
-    print(f"Train samples: {len(train_ds)} "
-        f"Val. samples: {len(eval_ds)}")
+    train_size = int(0.8*len(ds))
+    valid_size = len(ds) - train_size
+    train_set, val_set = random_split(ds, [train_size, valid_size])
+    print(f"Train samples: {len(train_set)} "
+        f"Val. samples: {len(val_set)}")
 
     # DataLoaders
-    pin_memory = False
-    train_dataloader = DataLoader(train_ds,
+    train_dataloader = DataLoader(train_set,
                         batch_size=batch_size,
                         shuffle=True,
-                        pin_memory=pin_memory,
+                        pin_memory=True,
                         num_workers=num_workers)
-    eval_dataloader = DataLoader(eval_ds,
-                        batch_size=batch_size,
+    eval_dataloader = DataLoader(val_set,
+                        batch_size=batch_size * 2,
                         shuffle=False,
-                        pin_memory=pin_memory,
+                        pin_memory=True,
                         num_workers=num_workers)
     return train_dataloader, eval_dataloader
 
-@profile
+
+# ## Training Loop
+
+# In[7]:
+
+
 def training_loop(dataset: str,
                   mixed_precision: str="fp16",
                   seed: int=123,
@@ -79,22 +129,6 @@ def training_loop(dataset: str,
     print(f"Args: {mixed_precision}, {seed}, {batch_size}, "
           f"{gradient_accumulation_steps}, {nb_epochs}, {train_mode}")
     
-
-    # Metadata
-    metadata_file = "/notebooks/data/metadata_parquet/features_metadata_slim.parquet"
-    metadata_df = pd.read_parquet(metadata_file)
-    chip_ids = metadata_df[metadata_df.split == "train"].chip_id.unique().tolist()
-
-    artifacts_dir = "/notebooks/artifacts"
-    model_name = "UTAE"
-    date = "20230118"
-    pretrained_weights_path = artifacts_dir + "/pretrained_utae/f1model.pth.tar"  # for fine tuning
-
-    saved_state_path = artifacts_dir + "/20230112_UTAE_S2_B32_E20.pt"  # for resuming training
-    save_path = artifacts_dir + (f"/{date}_{model_name}_{dataset}_B"
-        f"{batch_size * gradient_accumulation_steps}.pt")
-    best_model_path = save_path[:-3] + "_BEST.pt"
-
     # Set random seed
     set_seed(seed)
     
@@ -109,7 +143,7 @@ def training_loop(dataset: str,
         # gradient_accumulation_steps=gradient_accumulation_steps)
 
     # Build DataLoaders
-    train_dataloader, eval_dataloader = get_dataloaders(chip_ids, dataset, batch_size=batch_size)
+    train_dataloader, eval_dataloader = get_dataloaders(dataset, batch_size=batch_size)
 
     # Assign model inputs based on dataset
     if dataset == "Sentinel-1A":
@@ -135,7 +169,7 @@ def training_loop(dataset: str,
     else:
         model = UTAE(input_nc)  # modify output layer to predict AGBM
         if train_mode == "resume":
-            state_dict = torch.load(saved_state_path)  # , map_location=accelerator.device)
+            state_dict = torch.load(saved_state_path, map_location=accelerator.device)
             model.load_state_dict(state_dict)
     
     model = model.to(device)
@@ -157,17 +191,16 @@ def training_loop(dataset: str,
         epoch_start = time()
         # accelerator.print(f"Training")
         print(f"Training")
-        for b, batch in enumerate(tqdm(train_dataloader)):
+        for batch in tqdm(train_dataloader):
             # with accelerator.accumulate(model):
-            inputs, targets, _ = batch
-            outputs = model(inputs.to(device))
-            loss = loss_function(outputs, targets.to(device))
+            optimizer.zero_grad()
+            inputs = batch["image"].to(device)
+            targets = batch["target"].to(device)
+            outputs = model(inputs)
+            loss = loss_function(outputs, targets)
             # accelerator.backward(loss)
             loss.backward()
             optimizer.step()
-            optimizer.zero_grad()
-            if b == 2:
-                break
 
         epoch_end = time()
         # accelerator.print(f"  Training time: {epoch_end - epoch_start}")
@@ -186,17 +219,16 @@ def training_loop(dataset: str,
         num_elements = 0
         # accelerator.print(f"Validation")
         print(f"Validation")
-        for b, batch in enumerate(tqdm(eval_dataloader)):
-            inputs, targets, _ = batch
+        for batch in tqdm(eval_dataloader):
+            inputs = batch["image"].to(device)
+            targets = batch["target"].to(device)
             with torch.no_grad():
-                predictions = model(inputs.to(device))
+                predictions = model(inputs)
             # Gather all predictions and targets
             # all_predictions, all_targets = accelerator.gather_for_metrics((predictions, targets))
             # num_elements += all_predictions.shape[0]
             # val_loss += loss_function(all_predictions, all_targets).item()
-            val_loss += loss_function(predictions, targets.to(device)).item()
-            if b == 2:
-                break
+            val_loss += loss_function(predictions, targets).item()
 
         val_loss /= len(eval_dataloader)
         val_rmse = np.round(np.sqrt(val_loss), 5)
@@ -215,16 +247,42 @@ def training_loop(dataset: str,
             torch.save(model.state_dict(), best_model_path)
             print(f"  Best Model file path: {best_model_path}")
 
-if __name__ == "__main__":
-    dataset = "Sentinel-2all"
-    mixed_precision = "fp16"
-    seed = 123
-    batch_size = 8
-    gradient_accumulation_steps = 4
-    nb_epochs = 1
-    train_mode = "tune"
 
-    train_args = (dataset, mixed_precision, seed, batch_size,
-                  gradient_accumulation_steps, nb_epochs)
-    training_loop(*train_args)
+# In[8]:
 
+
+dataset = "Sentinel-2all"
+mixed_precision = "fp16"
+seed = 123
+batch_size = 8
+gradient_accumulation_steps = 4
+nb_epochs = 10
+train_mode = "tune"
+
+artifacts_dir = "/notebooks/artifacts"
+model_name = "UTAE"
+date = "20230117"
+pretrained_weights_path = artifacts_dir + "/pretrained_utae/f1model.pth.tar"  # for fine tuning
+saved_state_path = artifacts_dir + "/20230112_UTAE_S2_B32_E20.pt"  # for resuming training
+
+save_path = artifacts_dir + (f"/{date}_{model_name}_{dataset}_B"
+        f"{batch_size * gradient_accumulation_steps}.pt")
+best_model_path = save_path[:-3] + "_BEST.pt"
+
+train_args = (dataset, mixed_precision, seed, batch_size, gradient_accumulation_steps, nb_epochs, train_mode)
+# notebook_launcher(training_loop, train_args, num_processes=1)
+training_loop(*train_args)
+
+
+# In[ ]:
+
+
+print(train_args)
+
+##### Save the metrics to a file
+train_metrics_zipped = list(zip(np.arange(0, len(train_metrics)), train_metrics))
+metrics = {'training': train_metrics_zipped, 'validation': val_metrics}
+train_metrics_df = pd.DataFrame(metrics['training'], columns=["step", "score"])
+val_metrics_df = pd.DataFrame(metrics["validation"], columns=["step", "score"])
+train_metrics_df.to_csv(artifacts_dir + "/train_metrics.csv")
+val_metrics_df.to_csv(artifacts_dir + "/val_metrics.csv")
